6a88e49004a6e4c9b52909d2c356faa25e9a6080
Axel Fernández <axl1210@estudiantec.cr>
2021-10-25 23:26:53 -0600

    start app

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.css";
+
+import * as yup from "yup";
+
+import express, { Application, NextFunction, Request, Response } from "express";
+
+import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom";
+import cors from "cors";
+import helmet from "helmet";
+import nanoid from "nanoid";
+import reportWebVitals from "./reportWebVitals";
+
+const app: Application = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+const schema = yup.object().shape({
+  dest: yup.string().trim().url().required(),
+  slug: yup.string().trim(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
